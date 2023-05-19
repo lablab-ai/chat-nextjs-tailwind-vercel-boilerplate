ff895f7d55d78e6ee692cb723606fcaf0f8650e3
fabi.s <trapffm@web.de>
2023-05-19 10:37:51 +0200

    add messages logic

--- pages/index.js
+++ pages/index.js
@@ -1,142 +1,82 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Link from 'next/link';
-
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  // Declare a state variable called "messages" and initialize it with an empty array
+  const [messages, setMessages] = useState([]);
+
+  // Declare a state variable called "inputMessage" and initialize it with an empty string
+  const [inputMessage, setInputMessage] = useState("");
+
+  // Use the useEffect hook to update the messages array with the initial conversation
+  useEffect(() => {
+    setMessages([
+      {
+        sender: "user",
+        message: "Hello",
+      },
+      {
+        sender: "bot",
+        message: "Hi, how are you?",
+      },
+    ]);
+  }, []);
+
+  // Function to handle the sending of messages when the user presses the Enter key
+  const handleSendMessage = (e) => {
+    // Check if the pressed key is "Enter" and there is a non-empty message in inputMessage
+    if (e.key === "Enter" && inputMessage) {
+      // Add the user's message and a bot response to the messages array
+      setMessages([
+        ...messages,
+        { sender: "user", message: inputMessage },
+        { sender: "bot", message: "Nice!" },
+      ]);
+      // Reset the input message field
+      setInputMessage("");
+    }
+  };
+
+  // Render the main div and its contents, including the messages and input field
   return (
     <>
-    
       <body class="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
-      <Link href="/help">Get help with NextJS here</Link>
         <div class="flex flex-col flex-grow w-full max-w-4xl bg-white shadow-xl rounded-lg overflow-hidden">
           <div class="flex flex-col flex-grow h-0 p-4 overflow-auto">
-            <div class="flex w-full mt-2 space-x-3 max-w-xs">
-              <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-              <div>
-                <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
-                  <p class="text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
+            {/* Iterate over the messages array and render each message based on the sender */}
+            {messages.map((message) => {
+              // If the message sender is the user, render it differently than if the sender is a bot
+              return message.sender === "user" ? (
+                <div class="flex w-full mt-2 space-x-3 max-w-xs">
+                  <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+                  <div>
+                    <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+                      <p class="text-sm">{message.message}</p>
+                    </div>
+                  </div>
                 </div>
-                <span class="text-xs text-gray-500 leading-none">
-                  2 min ago
-                </span>
-              </div>
-            </div>
-            <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-              <div>
-                <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-                  <p class="text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod.
-                  </p>
+              ) : (
+                <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+                  <div>
+                    <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                      <p class="text-sm">{message.message}</p>
+                    </div>
+                  </div>
+                  <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
                 </div>
-                <span class="text-xs text-gray-500 leading-none">
-                  2 min ago
-                </span>
-              </div>
-              <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-            </div>
-            <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-              <div>
-                <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-                  <p class="text-sm">Lorem ipsum dolor sit amet.</p>
-                </div>
-                <span class="text-xs text-gray-500 leading-none">
-                  2 min ago
-                </span>
-              </div>
-              <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-            </div>
-            <div class="flex w-full mt-2 space-x-3 max-w-xs">
-              <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-              <div>
-                <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
-                  <p class="text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.{" "}
-                  </p>
-                </div>
-                <span class="text-xs text-gray-500 leading-none">
-                  2 min ago
-                </span>
-              </div>
-            </div>
-            <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-              <div>
-                <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-                  <p class="text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.{" "}
-                  </p>
-                </div>
-                <span class="text-xs text-gray-500 leading-none">
-                  2 min ago
-                </span>
-              </div>
-              <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-            </div>
-            <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-              <div>
-                <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-                  <p class="text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt.
-                  </p>
-                </div>
-                <span class="text-xs text-gray-500 leading-none">
-                  2 min ago
-                </span>
-              </div>
-              <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-            </div>
-            <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-              <div>
-                <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-                  <p class="text-sm">Lorem ipsum dolor sit amet.</p>
-                </div>
-                <span class="text-xs text-gray-500 leading-none">
-                  2 min ago
-                </span>
-              </div>
-              <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-            </div>
-            <div class="flex w-full mt-2 space-x-3 max-w-xs">
-              <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-              <div>
-                <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
-                  <p class="text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.{" "}
-                  </p>
-                </div>
-                <span class="text-xs text-gray-500 leading-none">
-                  2 min ago
-                </span>
-              </div>
-            </div>
-            <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-              <div>
-                <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-                  <p class="text-sm">Lorem ipsum dolor sit.</p>
-                </div>
-                <span class="text-xs text-gray-500 leading-none">
-                  2 min ago
-                </span>
-              </div>
-              <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-            </div>
+              );
+            })}
           </div>
-
+          {/* Render the footer section with an input field for typing messages */}
           <div class="bg-gray-300 p-4">
             <input
               class="flex items-center h-10 w-full rounded px-3 text-sm"
               type="text"
               placeholder="Type your message…"
+              value={inputMessage}
+              // Update the inputMessage state variable when the input field value changes
+              onChange={(e) => setInputMessage(e.target.value)}
+              // Call the handleSendMessage function when the user presses a key in the input field
+              onKeyDown={handleSendMessage}
             />
           </div>
         </div>
